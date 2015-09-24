c7532496bd324b30c59bc69283ff163309a94c79
ianng <iann@gorrill.com>
2015-09-24 14:33:00 -0600

    typo

--- package.js
+++ package.js
@@ -9,11 +9,11 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('bevanhunt:leaflet@0.3.18', 'client');
-  api.use('ianng:leaflet-control-geocoder@1.0.0', 'client');
+  api.use('ianng:leaflet-routing-machine@1.0.1', 'client');
   api.addFiles('ianng:Control.Geocoder.js', 'client');
   api.addFiles('ianng:Control.Geocoder.css', 'client');
-  api.addFiles('images/geocoder.png', 'client');
-  api.addFiles('images/throbber.gif', 'client');
+  api.addAssets('images/geocoder.png', 'client');
+  api.addAssets('images/throbber.gif', 'client');
 });
 
 
